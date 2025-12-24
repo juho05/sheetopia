80fc69217d4ba11c2f5234a61f43f28fb4d998df
Julian Hofmann <git@julianh.de>
2025-12-24 16:33:49 +0100

    Implement score grid cells

---
+++ lib/ui/common/text_scroll.dart
@@ -0,0 +1,628 @@
+// modified version of:
+// https://github.com/yurii-khi/text_scroll/blob/dev/lib/text_scroll.dart
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+/// TextScroll widget automatically scrolls provided [text] according
+/// to custom settings, in order to achieve 'marquee' text effect.
+///
+/// ### Example:
+///
+/// ```dart
+/// TextScroll(
+///     'This is the sample text for Flutter TextScroll widget. ',
+///     mode: TextScrollMode.bouncing,
+///     velocity: Velocity(pixelsPerSecond: Offset(150, 0)),
+///     delayBefore: Duration(milliseconds: 500),
+///     numberOfReps: 5,
+///     pauseBetween: Duration(milliseconds: 50),
+///     style: TextStyle(color: Colors.green),
+///     textAlign: TextAlign.right,
+///     selectable: true,
+/// )
+/// ```
+class TextScroll extends StatefulWidget {
+  const TextScroll(
+    this.text, {
+    super.key,
+    this.style,
+    this.textAlign,
+    this.textDirection = TextDirection.ltr,
+    this.numberOfReps,
+    this.delayBefore = const Duration(seconds: 3),
+    this.pauseBetween = const Duration(seconds: 3),
+    this.pauseOnBounce,
+    this.mode = TextScrollMode.endless,
+    this.velocity = const Velocity(pixelsPerSecond: Offset(40, 0)),
+    this.selectable = false,
+    this.intervalSpaces = 5,
+    this.fadedBorder = true,
+    this.fadedBorderWidth = 0.025,
+    this.fadeBorderSide = FadeBorderSide.right,
+    this.fadeBorderVisibility = FadeBorderVisibility.auto,
+  });
+
+  /// The text string, that would be scrolled.
+  /// In case text does fit into allocated space, it wouldn't be scrolled
+  /// and would be shown as is.
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll('This is the sample text for Flutter TextScroll widget.')
+  /// ```
+  final String text;
+
+  /// Provides [TextAlign] alignment if text string is not long enough to be scrolled.
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Short string',
+  ///   textAlign: TextAlign.right,
+  /// )
+  /// ```
+  final TextAlign? textAlign;
+
+  /// Provides [TextDirection] - a direction in which text flows.
+  /// Default is [TextDirection.ltr].
+  /// Default scrolling direction would be opposite to [textDirection],
+  /// e.g. for [TextDirection.rtl] scrolling would be from left to right
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Hey! I\'m a RTL text, check me out. Hey! I\'m a RTL text, check me out. Hey! I\'m a RTL text, check me out. ',
+  ///   textDirection: TextDirection.rtl,
+  /// )
+  /// ```
+  final TextDirection textDirection;
+
+  /// Allows to apply custom [TextStyle] to [text].
+  ///
+  /// `null` by default.
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Styled text',
+  ///   style: TextStyle(
+  ///     color: Colors.red,
+  ///   ),
+  /// )
+  /// ```
+  final TextStyle? style;
+
+  /// Limits number of scroll animation rounds.
+  ///
+  /// Default is [infinity].
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Limit scroll rounds to 10',
+  ///   numberOfReps: 10,
+  /// )
+  /// ```
+  final int? numberOfReps;
+
+  /// Delay before first animation round.
+  ///
+  /// Default is [Duration.zero].
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Start animation after 1 sec delay',
+  ///   delayBefore: Duration(seconds: 1),
+  /// )
+  /// ```
+  final Duration? delayBefore;
+
+  /// Determines pause interval between animation rounds.
+  ///
+  /// Default is [Duration.zero].
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Pause animation between rounds',
+  ///   mode: TextScrollMode.bouncing,
+  ///   pauseBetween: Duration(milliseconds: 300),
+  /// )
+  /// ```
+  final Duration? pauseBetween;
+
+  /// Determines pause interval before changing direction on a bounce.
+  ///
+  /// Default is [Duration.zero].
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Pause animation when text reaches the end',
+  ///   mode: TextScrollMode.bouncing,
+  ///   pauseOnBounce: Duration(milliseconds: 300),
+  /// )
+  /// ```
+  final Duration? pauseOnBounce;
+
+  /// Sets one of two different types of scrolling behavior.
+  ///
+  /// [TextScrollMode.endless] - default, scrolls text in one direction endlessly.
+  ///
+  /// [TextScrollMode.bouncing] - when [text] string is scrolled to its end,
+  /// starts animation to opposite direction.
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Animate text string back and forth',
+  ///   mode: TextScrollMode.bouncing,
+  /// )
+  /// ```
+  final TextScrollMode mode;
+
+  /// Allows to customize animation speed.
+  ///
+  /// Default is `Velocity(pixelsPerSecond: Offset(80, 0))`
+  ///
+  /// ### Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Animate text at 200px per second',
+  ///   velocity: Velocity(pixelsPerSecond: Offset(200, 0)),
+  /// )
+  final Velocity velocity;
+
+  /// Allows users to select provided [text], copy it to clipboard etc.
+  ///
+  /// Default is `false`.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'Selectable text',
+  ///   selectable: true,
+  /// )
+  /// ```
+  final bool selectable;
+
+  /// Adds blank spaces between two nearby text sentences
+  /// in case of [TextScrollMode.endless]
+  ///
+  /// Default is `1`.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'This is the sample text for Flutter TextScroll widget. ',
+  ///   blankSpaces: 10,
+  /// )
+  /// ```
+  final int? intervalSpaces;
+
+  /// Fades the text out to the left and right edges of the widget.
+  /// Default is `false`.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'This is the sample text for Flutter TextScroll widget. ',
+  ///   fadedBorder: true,
+  /// )
+  /// ```
+  final bool fadedBorder;
+
+  /// Sets width of the faded border.
+  /// Default is `0.2`.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///   'This is the sample text for Flutter TextScroll widget. ',
+  ///   fadedBorder: true,
+  ///   fadedBorderWidth: 0.25,
+  /// )
+  /// ```
+  final double? fadedBorderWidth;
+
+  /// Sets which side of the widget should be faded.
+  /// Default is [FadeBorderSide.both].
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///  'This is the sample text for Flutter TextScroll widget. ',
+  ///  fadedBorder: true,
+  ///  fadeBorderSide: FadeBorderSide.left,
+  ///  )
+  ///  ```
+  final FadeBorderSide fadeBorderSide;
+
+  /// Sets when the fadeBorder should be shown.
+  /// Default is [FadeBorderSide.auto].
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// TextScroll(
+  ///  'This is the sample text for Flutter TextScroll widget. ',
+  ///  fadedBorder: true,
+  ///  fadeBorderVisibility: FadeBorderVisibility.always,
+  ///  )
+  ///  ```
+  final FadeBorderVisibility fadeBorderVisibility;
+
+  @override
+  State<TextScroll> createState() => _TextScrollState();
+}
+
+class _TextScrollState extends State<TextScroll> {
+  final _scrollController = ScrollController();
+  String? _endlessText;
+  double? _originalTextWidth;
+  double _textMinWidth = 0;
+  Timer? _timer;
+  bool _running = false;
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) => _initScroller(null));
+  }
+
+  @override
+  void didUpdateWidget(covariant TextScroll oldWidget) {
+    if (widget.text != oldWidget.text ||
+        oldWidget.velocity != widget.velocity ||
+        widget.mode != oldWidget.mode) {
+      _initScroller(_lastWidth);
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _resetId++;
+    _scrollController.dispose();
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  double? _lastWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    assert(
+      widget.intervalSpaces == null || widget.mode == TextScrollMode.endless,
+      'intervalSpaces is only available in TextScrollMode.endless mode',
+    );
+    assert(
+      !widget.fadedBorder ||
+          (widget.fadedBorder &&
+              widget.fadedBorderWidth != null &&
+              widget.fadedBorderWidth! > 0 &&
+              widget.fadedBorderWidth! <= 1),
+      'fadedBorderInterval must be between 0 and 1 when fadedBorder is true',
+    );
+
+    Widget baseWidget = Directionality(
+      textDirection: widget.textDirection,
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minWidth: _textMinWidth),
+          child: widget.selectable
+              ? SelectableText(
+                  _endlessText ?? widget.text,
+                  style: widget.style,
+                  textAlign: widget.textAlign,
+                )
+              : Text(
+                  _endlessText ?? widget.text,
+                  style: widget.style,
+                  textAlign: widget.textAlign,
+                ),
+        ),
+      ),
+    );
+
+    /// Used to add the fade border effect, if enabled
+    Widget? fadeBorderWidget;
+
+    /// If fade border is enabled
+    if (widget.fadedBorder) {
+      ///Fill list with amount of transparent colors to make the text visible
+      final List<Color> colors = List.generate(
+        1 ~/ widget.fadedBorderWidth! - 1,
+        (index) {
+          return Colors.transparent;
+        },
+        growable: true,
+      );
+
+      ///Add black color to add gradient fade out
+      if (widget.fadeBorderSide == FadeBorderSide.both ||
+          widget.fadeBorderSide == FadeBorderSide.left) {
+        colors.insert(0, Colors.black);
+      } else {
+        colors.add(Colors.transparent);
+      }
+      if (widget.fadeBorderSide == FadeBorderSide.both ||
+          widget.fadeBorderSide == FadeBorderSide.right) {
+        colors.add(Colors.black);
+      } else {
+        colors.add(Colors.transparent);
+      }
+
+      ///Calculate the stops for the gradient
+      final List<double> stops = List.generate(1 ~/ widget.fadedBorderWidth!, (
+        index,
+      ) {
+        return (index + 1) * widget.fadedBorderWidth!;
+      }, growable: true);
+
+      ///Add first stop to list
+      stops.insert(0, 0);
+
+      /// Pre-render text to get it's width
+      final TextPainter textPrototype = TextPainter(
+        text: TextSpan(text: _endlessText ?? widget.text, style: widget.style),
+        textDirection: widget.textDirection,
+        textScaler: MediaQuery.of(context).textScaler,
+        textWidthBasis: TextWidthBasis.longestLine,
+      )..layout();
+
+      ///Apply ShaderMask to the text
+      fadeBorderWidget = LayoutBuilder(
+        builder: (context, constraints) {
+          ///When text is wider than the widget, apply ShaderMask
+          if (widget.fadeBorderVisibility == FadeBorderVisibility.always ||
+              constraints.maxWidth < textPrototype.size.width) {
+            return ShaderMask(
+              blendMode: BlendMode.dstOut,
+              shaderCallback: (rect) {
+                return LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: colors,
+                  stops: stops,
+                ).createShader(rect);
+              },
+              child: baseWidget,
+            );
+          } else {
+            ///When text is smaller than the widget, just return the text
+            return baseWidget;
+          }
+        },
+      );
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (_lastWidth != null && constraints.maxWidth != _lastWidth) {
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            _initScroller(constraints.maxWidth);
+          });
+        }
+        _lastWidth = constraints.maxWidth;
+        return fadeBorderWidget ?? baseWidget;
+      },
+    );
+  }
+
+  int _resetId = 0;
+  Future<void> _initScroller(double? width) async {
+    int resetId = ++_resetId;
+    _timer?.cancel();
+
+    if (_available) {
+      _scrollController.jumpTo(0);
+    }
+
+    setState(() {
+      _textMinWidth = width ?? _scrollController.position.viewportDimension;
+      _endlessText = null;
+      _counter = 0;
+    });
+
+    await _delayBefore();
+    if (resetId != _resetId) return;
+    _setTimer();
+  }
+
+  /// Sets [_timer] for animation
+  void _setTimer() {
+    ///Cancel previous timer if it exists
+    _timer?.cancel();
+
+    ///Reset [_running] to allow for updates on changed velocity
+    _running = false;
+
+    _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      if (!_available) {
+        _timer?.cancel();
+        _timer = null;
+        return;
+      }
+
+      final int? maxReps = widget.numberOfReps;
+      if (maxReps != null && _counter >= maxReps) {
+        _timer?.cancel();
+        _timer = null;
+        return;
+      }
+
+      if (!_running) _run();
+    });
+  }
+
+  Future<void> _run() async {
+    _running = true;
+
+    final int? maxReps = widget.numberOfReps;
+    if (maxReps == null || _counter < maxReps) {
+      _counter++;
+
+      switch (widget.mode) {
+        case TextScrollMode.bouncing:
+          {
+            await _animateBouncing();
+            break;
+          }
+        default:
+          {
+            await _animateEndless();
+          }
+      }
+    }
+    _running = false;
+  }
+
+  Future<void> _animateEndless() async {
+    if (!_available) return;
+    int resetId = _resetId;
+
+    final ScrollPosition position = _scrollController.position;
+    final bool needsScrolling = position.maxScrollExtent > 0;
+    if (!needsScrolling) {
+      if (_endlessText != null) setState(() => _endlessText = null);
+      return;
+    }
+
+    if (_endlessText == null || _originalTextWidth == null) {
+      setState(() {
+        _originalTextWidth =
+            position.maxScrollExtent + position.viewportDimension;
+        _endlessText =
+            widget.text + _getSpaces(widget.intervalSpaces ?? 1) + widget.text;
+      });
+
+      return;
+    }
+
+    final double endlessTextWidth =
+        position.maxScrollExtent + position.viewportDimension;
+    final double singleRoundExtent = endlessTextWidth - _originalTextWidth!;
+    final Duration duration = _getDuration(singleRoundExtent);
+    if (duration == Duration.zero) return;
+
+    if (!_available || resetId != _resetId) return;
+    await _scrollController.animateTo(
+      singleRoundExtent,
+      duration: duration,
+      curve: Curves.linear,
+    );
+    if (!_available || resetId != _resetId) return;
+    _scrollController.jumpTo(position.minScrollExtent);
+
+    ///Pause between animation rounds
+    if (widget.pauseBetween != null && resetId == _resetId) {
+      await Future.delayed(widget.pauseBetween!);
+    }
+  }
+
+  Future<void> _animateBouncing() async {
+    int resetId = _resetId;
+
+    final double maxExtent = _scrollController.position.maxScrollExtent;
+    final double minExtent = _scrollController.position.minScrollExtent;
+    final double extent = maxExtent - minExtent;
+    final Duration duration = _getDuration(extent);
+    if (duration == Duration.zero) return;
+
+    if (!_available || resetId != _resetId) return;
+    await _scrollController.animateTo(
+      maxExtent,
+      duration: duration,
+      curve: Curves.linear,
+    );
+    if (widget.pauseOnBounce != null && resetId == _resetId) {
+      await Future.delayed(widget.pauseOnBounce!);
+    }
+    if (!_available || resetId != _resetId) return;
+    await _scrollController.animateTo(
+      minExtent,
+      duration: duration,
+      curve: Curves.linear,
+    );
+    if (!_available || resetId != _resetId) return;
+    if (widget.pauseBetween != null && resetId == _resetId) {
+      await Future<dynamic>.delayed(widget.pauseBetween!);
+    }
+  }
+
+  Future<void> _delayBefore() async {
+    final Duration? delayBefore = widget.delayBefore;
+    if (delayBefore == null) return;
+
+    await Future<dynamic>.delayed(delayBefore);
+  }
+
+  Duration _getDuration(double extent) {
+    ///No movement when velocity offset dx equals 0
+    if (widget.velocity.pixelsPerSecond.dx == 0) return Duration.zero;
+
+    final int milliseconds =
+        (extent * 1000 / widget.velocity.pixelsPerSecond.dx).round();
+
+    return Duration(milliseconds: milliseconds);
+  }
+
+  void _onUpdate(TextScroll oldWidget) {}
+
+  String _getSpaces(int number) {
+    String spaces = '';
+    for (int i = 0; i < number; i++) {
+      spaces += '\u{00A0}';
+    }
+
+    return spaces;
+  }
+
+  bool get _available => mounted && _scrollController.hasClients;
+}
+
+/// Animation types for [TextScroll] widget.
+///
+/// [endless] - scrolls text in one direction endlessly.
+///
+/// [bouncing] - when text is scrolled to its end,
+/// starts animation to opposite direction.
+enum TextScrollMode { bouncing, endless }
+
+/// Side of the text border to fade out.
+///
+/// [left] - fade out left side of the text.
+/// [right] - fade out right side of the text.
+/// [both] - fade out both sides of the text.
+enum FadeBorderSide { left, right, both }
+
+/// Sets when the fade border will be applied
+///
+/// [always] - always show the border, independent of the text length
+/// [auto] - show the border only when the text is wider than the widget
+enum FadeBorderVisibility { always, auto }
--- lib/ui/home/score_grid_cell.dart
+++ lib/ui/home/score_grid_cell.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sheetopia/data/repositories/scores/score.dart';
+import 'package:sheetopia/ui/common/common_badge.dart';
+import 'package:sheetopia/ui/common/optional_tooltip.dart';
+import 'package:sheetopia/ui/common/tag_badge.dart';
+import 'package:sheetopia/ui/common/text_scroll.dart';
 
 class ScoreGridCell extends StatelessWidget {
-  static const double width = 240;
-  static const double height = 310;
+  static const double width = 250;
+  static const double height = 288;
 
   final Score score;
 
@@ -11,6 +16,127 @@ class ScoreGridCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(width: width, height: height, child: Placeholder());
+    final theme = Theme.of(context);
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Material(
+        borderRadius: BorderRadius.circular(12),
+        color: theme.colorScheme.surfaceContainer,
+        elevation: 2,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12),
+          onTap: () {
+            context.go("/scores/${score.id}");
+          },
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 4,
+                children: [
+                  // TODO: replace with preview
+                  ClipRRect(
+                    borderRadius: const BorderRadiusGeometry.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
+                    ),
+                    child: Container(color: Colors.white, height: height / 2.1),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 4,
+                      children: [
+                        OptionalTooltip(
+                          message: score.title,
+                          child: TextScroll(
+                            score.title,
+                            style: theme.textTheme.bodyMedium!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        OptionalTooltip(
+                          message: score.composer,
+                          child: Text(
+                            score.composer ?? "No composer",
+                            overflow: TextOverflow.ellipsis,
+                            style: theme.textTheme.bodyMedium!.copyWith(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w300,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: SizedBox(
+                            height: 24,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: score.instruments.length,
+                              itemBuilder: (context, index) => Padding(
+                                padding: index == 0
+                                    ? EdgeInsets.zero
+                                    : const EdgeInsets.only(left: 4),
+                                child: CommonBadge(
+                                  name: score.instruments[index],
+                                  color:
+                                      theme.colorScheme.surfaceContainerHighest,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        if (score.tags.isNotEmpty) const Divider(),
+                        SizedBox(
+                          height: 24,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: score.tags.length,
+                            itemBuilder: (context, index) => Padding(
+                              padding: index == 0
+                                  ? EdgeInsets.zero
+                                  : const EdgeInsets.only(left: 4),
+                              child: TagBadge(tag: score.tags[index]),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: SizedBox.square(
+                    dimension: 34,
+                    child: IconButton.filled(
+                      color: Colors.white,
+                      iconSize: 20,
+                      padding: const EdgeInsets.all(0),
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                          Colors.black.withAlpha(100),
+                        ),
+                      ),
+                      onPressed: () {
+                        context.go("/scores/${score.id}/edit");
+                      },
+                      icon: const Icon(Icons.edit),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
