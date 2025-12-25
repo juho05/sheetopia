d07405d830a4ce96dbe9c4007accafb7653f4505
Julian Hofmann <git@julianh.de>
2025-12-25 17:24:10 +0100

    Setup sharing extension on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_sharing_intent
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,5 +10,17 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+
+    let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
+    /// if the url is made from SwiftFlutterSharingIntentPlugin then handle it with plugin [SwiftFlutterSharingIntentPlugin]
+    if sharingIntent.hasSameSchemePrefix(url: url) {
+        return sharingIntent.application(app, open: url, options: options)
+    }
+
+    // Proceed url handling for other Flutter libraries like uni_links
+    return super.application(app, open: url, options:options)
   }
 }
---
+++ ios/Share Extension/ShareViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ShareViewController.swift
+//  Share Extension
+//
+//  Created by Julian Hofmann on 25.12.25.
+//
+
+class ShareViewController: FSIShareViewController {
+}
