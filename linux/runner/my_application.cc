e4eb6dd421b9dde64e6a5d23d3f77f213c6f4fe5
Julian Hofmann <git@julianh.de>
2026-07-22 15:21:51 +0200

    Change default window width

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -58,7 +58,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "Sheetopia");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 1220, 720);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
